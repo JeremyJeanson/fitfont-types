7c30f2cc7fd4fe1c4f3d4be062879a33e32fa103
Jeremy Jeanson <jeremy.jeanson@live.fr>
2020-10-03 00:47:34 +0200

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿[![npm](https://img.shields.io/npm/dw/fitfont-types.svg?logo=npm&label=npm%20version)](https://www.npmjs.com/package/fitfont-types)
+
+# Introduction
+This package include Types definitions to use [fitfont](https://github.com/gregoiresage/fitfont) with TypeScript.
+
+Types have comments to allow developers to view documentation in Visual Studio Code.
+
+# Installation
+## 1. Install the module
+You could use a any package manager to install this module. It was tested with npm, yarn and pnpm.
+```sh
+npm install fitfont-types --save-dev
+```
+## 2. Include types in your projects
+Update the `tsconfig.json` inside the app folder. Add `"../node_modules/fitfont-types/types"`  to paths to include.
+```json
+{
+ "extends": "../tsconfig.json",
+ "include": [
+  "**/*.ts",
+  "../node_modules/fitbit-sdk-types/types/device",
+  "../node_modules/fitfont-types/types"
+ ]
+}
+```
---
+++ types/app.d.ts
@@ -0,0 +1,74 @@
+declare module "fitfont" {
+    /**
+     * Horizontal alignment
+     */
+    type HorizontalAlign = "start" | "middle" | "end";
+
+    /**
+     * Vertical alignment
+     */
+    type VerticalAlign = "top" | "middle" | "bottom" | "baseline";
+
+    /**
+     * Option to initialize a fitfont class
+     */
+    interface Options {
+        /**
+         * Id fo the symbol or the Element to use
+         */
+        id: string | Element,
+        /**
+         * name of the generated font folder
+         */
+        font: string,
+        /**
+         * Horizontal alignment
+         */
+        halign?: HorizontalAlign,
+        /**
+         * Vertical alignment
+         */
+        valign?: VerticalAlign,
+        /**
+         * Letter spacing
+         */
+        letterspacing?: number
+    }
+
+    /**
+     * Fitfont class to use cutom fonts
+     */
+    class FitFont {
+        constructor(options: Options);
+
+        /**
+         * Force the redraw
+         */
+        redraw(): void;
+
+        /**
+         * Text to show
+         */
+        text: string;
+
+        /**
+         * Horizontal alignment
+         */
+        halign: HorizontalAlign;
+
+        /**
+         * Vertical alignment
+         */
+        valign: VerticalAlign;
+
+        /**
+         * Letter spacing
+         */
+        letterspacing: number;
+
+        /**
+         * With (read only)
+         */
+        readonly width: number;
+    }
+}
